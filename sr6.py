f05707f74d0e3811132e9be3d879a72cd530b082
franco611 <62269078+franco611@users.noreply.github.com>
2022-09-02 03:35:29 -0600

    sr6Final

--- sr6.py
+++ sr6.py
@@ -11,7 +11,31 @@ glColor(1,1,0)
 t = Textures("cofre.bmp")
 glTexture(t)
 glViewPort(0,0,1500,1500)
-glLook((1,1,1),(0,1,0),(0,1,0))
-#glObjeto3D('cofre.obj', (850,850,800),(1000,900,600), (0.4,0.4,0))
-glObjeto3D('cofre.obj', (0.7, 0.7, 0.7),(0,0,0), (0.1,0.9,0))
-glFinish('sr6.bmp')
+
+'''
+#Medium Shot
+glLook((1.2,1.7,1),(0,1.5,0),(0,1,0))
+glObjeto3D('cofre.obj', (1.5, 1.5, 1.5),(-0.3,0,0), (-0.02,-0.4,0))
+glFinish('sr6MediumShot.bmp')
+'''
+
+'''
+#Low angle Shot
+glLook((1.2,1,1),(0,1.5,0),(0,1,0))
+glObjeto3D('cofre.obj', (1.5, 1.5, 1.5),(-0.3,0,0), (-0.02,-0.4,0))
+glFinish('sr6LowShot.bmp')
+'''
+
+'''
+#High Shot
+glLook((1.2,3,1),(0,1.5,0),(0,1,0))
+glObjeto3D('cofre.obj', (1.5, 1.5, 1.5),(-0.3,0,0), (-0.02,-0.4,0))
+glFinish('sr6HighShot.bmp')
+'''
+
+'''
+#Dutch Shot
+glLook((1.2,1.7,1),(0,1.7,0.55),(-0.8,1,0))
+glObjeto3D('cofre.obj', (1.5, 1.5, 1.5),(-0.3,0,0), (-0.02,-0.4,0))
+glFinish('sr6DutchShot.bmp')
+'''

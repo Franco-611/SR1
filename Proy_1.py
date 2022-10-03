44d9f99745ed4305bee1c0942082c9124160bfb9
franco611 <62269078+franco611@users.noreply.github.com>
2022-10-02 21:04:32 -0600

    Mejoras

--- Proy_1.py
+++ Proy_1.py
@@ -18,12 +18,12 @@ glLook((0,0,1),(0,0,0),(0,1,0))
 
 t = Textures("cofre.bmp")
 glTexture(t)
-glObjeto3D('cofre.obj', (0.15, 0.15, 0.15),(-0.7,-0.8,0), (0.35,0.3,0))
+glObjeto3D('cofre.obj', (0.15, 0.15, 0.15),(-0.7,-0.8,-0.6), (0.35,0.3,0))
 
 glNuevo()
 t = Textures("silla.bmp")
 glTexture(t)
-glObjeto3D('silla.obj', (0.1, 0.1, 0.1),(-0.68,-0.67,-0.4), (0.2,0.3,0))
+glObjeto3D('silla.obj', (0.1, 0.1, 0.1),(-0.68,-0.67,-0.6), (0.2,0.3,0))
 
 glNuevo()
 t = Textures("ave.bmp")
@@ -33,8 +33,11 @@ glObjeto3D('ave.obj', (0.07, 0.07, 0.07),(-0.75,-0.15, -0.3), (0.3,0.8,0))
 glNuevo()
 t = Textures("barril.bmp")
 glTexture(t)
-glObjeto3D('barril.obj', (0.14, 0.14, 0.14),(-0.94, -0.75, -0.5), (0.2,0.3,0))
+glObjeto3D('barril.obj', (0.14, 0.14, 0.14),(-0.94, -0.75, -0.8), (0.2,0.3,0))
 
+#glNuevo()
+#t = Textures("pirata.bmp")
+#glTexture(t)
 #glObjeto3D('pirata.obj', (1.05, 1.05, 1.05),(-0.5,-0.5,0), (0,0,0))
 
 
